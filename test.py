37c24536c239cab1987d3416350dc980720dbeed
Ujjwal <ujjwalrajput3107@gmail.com>
2022-07-23 17:28:19 +0530

    this is my first push in git

---
+++ test.py
@@ -0,0 +1,14 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://ujjwall:Ujjwal2609@cluster0.3y5ya.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d={
+    "name":"Ujjwal",
+    "mail_id" : "ujjwalrajput3107@gmail.com",
+    "surname": "rajput"
+}
+db1=client["mongotest"]
+coll=db1['test']
+coll.insert_one(d)
+
+
